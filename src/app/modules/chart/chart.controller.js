9ea7a9f1fc4ad7f635329a9b0fde92f3389ffd39
Duongkhue <duongkhue_c6@yahoo.com>
2015-05-19 09:40:45 +0700

    Fix show chart

--- src/app/modules/chart/chart.controller.js
+++ src/app/modules/chart/chart.controller.js
@@ -1,17 +1,19 @@
 'use strict';
 function ChartCtrl(){
-    /*var vm = this;
+    var vm = this;
     vm.title = 'Chart';
-    */
-    c3.generate({
-        bindto: '#chart',
-        data: {
-            columns: [
-                ['data1', 30, 200, 100, 400, 150, 250],
-                ['data2', 50, 20, 10, 40, 15, 25]
-            ]
-        }
-    });
+
+    vm.showGraph= function() {
+        c3.generate({
+            bindto: '#chart',
+            data: {
+                columns: [
+                    ['data1', 30, 200, 100, 400, 150, 250],
+                    ['data2', 50, 20, 10, 40, 15, 25]
+                ]
+            }
+        });
+    }
 }
 angular.module('myproject')
     .controller('ChartCtrl',ChartCtrl);
