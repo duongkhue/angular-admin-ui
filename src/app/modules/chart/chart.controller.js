40d588bac91773f41d5fd84be4479d95ab1cce84
Duongkhue <duongkhue_c6@yahoo.com>
2015-05-19 16:57:22 +0700

    change id chart

--- src/app/modules/chart/chart.controller.js
+++ src/app/modules/chart/chart.controller.js
@@ -5,7 +5,7 @@ function ChartCtrl(){
 
     vm.showGraph= function() {
         c3.generate({
-            bindto: '#chart',
+            bindto: '#viewChart',
             data: {
                 columns: [
                     ['data1', 30, 200, 100, 400, 150, 250],
