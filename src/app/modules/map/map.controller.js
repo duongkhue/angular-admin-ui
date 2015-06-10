e67b3f1fb2410dbac4b55f05d326082f560d5c48
Duongkhue <duongkhue_c6@yahoo.com>
2015-06-10 16:45:04 +0700

    change marker in map

--- src/app/modules/map/map.controller.js
+++ src/app/modules/map/map.controller.js
@@ -12,10 +12,21 @@ function mapController($scope, $http, leafletData){
     var vm = this;
     vm.titlepage = 'Map';
     vm.address = '1600 Pennsylvania Avenue NW, Washington, D.C. 20500';
+
     angular.extend($scope, {
         center: {
-            autoDiscover: true,
-            zoom: 20
+            lat: 41.85,
+            lng: -87.65,
+            zoom: 8
+        },
+        markers: {
+            m1: {
+                lat: 41.85,
+                lng: -87.65,
+                message: "I'm a static marker with defaultIcon",
+                focus: false,
+                icon: {}
+            }
         },
         controls: {
             draw: {}
@@ -49,8 +60,345 @@ function mapController($scope, $http, leafletData){
         });
     });
 
+    vm.changeIcon = function(){
+        var params = [];
+        params['lat'] = 41.85;
+        params['lng'] = -87.65;
+        params['message'] = "I'm a static marker with defaultIcon";
+        params['focus'] = false;
+        switch(vm.iconMarker){
+            case 'leafIcon':
+                params['icon'] = {
+                    iconUrl : '../app/layout/images/leaf-green.png',
+                    shadowUrl: '../app/layout/images/leaf-shadow.png',
+                    iconSize: [38, 95],
+                    shadowSize: [50, 64],
+                    iconAnchor: [22, 94],
+                    shadowAnchor: [4, 62],
+                    popupAnchor: [-3, -76]
+                }
+                createMarker(params);
+                break;
+            case 'orangeLeafIcon':
+                params['icon'] = {
+                    iconUrl : '../app/layout/images/leaf-orange.png',
+                    shadowUrl: '../app/layout/images/leaf-shadow.png',
+                    iconSize: [38, 95],
+                    shadowSize: [50, 64],
+                    iconAnchor: [22, 94],
+                    shadowAnchor: [4, 62],
+                    popupAnchor: [-3, -76]
+                }
+                createMarker(params);
+                break;
+            case 'blueAwesomeMarkerIcon':
+                params['icon'] = {
+                    type : 'awesomeMarker',
+                    icon: 'tag',
+                    color: 'blue'
+                }
+                createMarker(params);
+                break;
+            case 'redAwesomeMarkerIcon':
+                params['icon'] = {
+                    type : 'awesomeMarker',
+                    icon: 'tag',
+                    color: 'red'
+                }
+                createMarker(params);
+                break;
+            case 'greenAwesomeMarkerIcon':
+                params['icon'] = {
+                    type : 'awesomeMarker',
+                    icon: 'tag',
+                    color: 'green'
+                }
+                createMarker(params);
+                break;
+            case 'tabAwesomeMarkerIcon':
+                params['icon'] = {
+                    type : 'awesomeMarker',
+                    icon: 'tag',
+                    color: 'red'
+                }
+                createMarker(params);
+                break;
+            case 'usdAwesomeMarkerIcon':
+                params['icon'] = {
+                    type : 'awesomeMarker',
+                    icon: 'usd',
+                    color: 'red'
+                }
+                createMarker(params);
+                break;
+            case 'heartAwesomeMarkerIcon':
+                params['icon'] = {
+                    type : 'awesomeMarker',
+                    icon: 'heart',
+                    color: 'red'
+                }
+                createMarker(params);
+                break;
+            case 'homeAwesomeMarkerIcon':
+                params['icon'] = {
+                    type : 'awesomeMarker',
+                    icon: 'home',
+                    color: 'red'
+                }
+                createMarker(params);
+                break;
+            case 'cogAwesomeMarkerIcon':
+                params['icon'] = {
+                    type : 'awesomeMarker',
+                    icon: 'cog',
+                    color: 'red'
+                }
+                createMarker(params);
+                break;
+            case 'starAwesomeMarkerIcon':
+                params['icon'] = {
+                    type : 'awesomeMarker',
+                    icon: 'star',
+                    color: 'red'
+                }
+                createMarker(params);
+                break;
+            case 'blueMakiMarkerIcon':
+                params['icon'] = {
+                    type : 'makiMarker',
+                    icon: 'beer',
+                    color: '12a',
+                    size: '1'
+                }
+                createMarker(params);
+                break;
+            case 'redMakiMarkerIcon':
+                params['icon'] = {
+                    type : 'makiMarker',
+                    icon: 'beer',
+                    color: 'f00',
+                    size: '1'
+                }
+                createMarker(params);
+                break;
+            case 'greenMakiMarkerIcon':
+                params['icon'] = {
+                    type : 'makiMarker',
+                    icon: 'beer',
+                    color: '0f0',
+                    size: '1'
+                }
+                createMarker(params);
+                break;
+            case 'purpleMakiMarkerIcon':
+                params['icon'] = {
+                    type : 'makiMarker',
+                    icon: 'beer',
+                    color: 'b0b',
+                    size: '1'
+                }
+                createMarker(params);
+                break;
+            case 'rocketMakiMarkerIcon':
+                params['icon'] = {
+                    type : 'makiMarker',
+                    icon: 'rocket',
+                    color: '#f00',
+                    size: '1'
+                }
+                createMarker(params);
+                break;
+            case 'beerMakiMarkerIcon':
+                params['icon'] = {
+                    type : 'makiMarker',
+                    icon: 'beer',
+                    color: '#f00',
+                    size: '1'
+                }
+                createMarker(params);
+                break;
+            case 'warehouseMakiMarkerIcon':
+                params['icon'] = {
+                    type : 'makiMarker',
+                    icon: 'warehouse',
+                    color: '#f00',
+                    size: '1'
+                }
+                createMarker(params);
+                break;
+            case 'shopMakiMarkerIcon':
+                params['icon'] = {
+                    type : 'makiMarker',
+                    icon: 'shop',
+                    color: '12a',
+                    size: '1'
+                }
+                createMarker(params);
+                break;
+            case 'blueExtraMarkerIcon':
+                params['icon'] = {
+                    type : 'extraMarker',
+                    icon: 'fa-star',
+                    color: 'blue-dark',
+                    size: '1',
+                    shape: 'circle',
+                    prefix: 'fa'
+                }
+                createMarker(params);
+                break;
+            case 'redExtraMarkerIcon':
+                params['icon'] = {
+                    type : 'extraMarker',
+                    icon: 'fa-star',
+                    color: 'red',
+                    size: '1',
+                    shape: 'circle',
+                    prefix: 'fa'
+                }
+                createMarker(params);
+                break;
+            case 'greenExtraMarkerIcon':
+                params['icon'] = {
+                    type : 'extraMarker',
+                    icon: 'fa-star',
+                    color: 'green',
+                    size: '1',
+                    shape: 'circle',
+                    prefix: 'fa'
+                }
+                createMarker(params);
+                break;
+            case 'tagExtraMarkerIcon':
+                params['icon'] = {
+                    type : 'extraMarker',
+                    icon: 'fa-tag',
+                    color: 'green',
+                    size: '1',
+                    shape: 'circle',
+                    prefix: 'fa'
+                }
+                createMarker(params);
+                break;
+            case 'usdExtraMarkerIcon':
+                params['icon'] = {
+                    type : 'extraMarker',
+                    icon: 'fa-usd',
+                    color: 'green',
+                    size: '1',
+                    shape: 'circle',
+                    prefix: 'fa'
+                }
+                createMarker(params);
+                break;
+            case 'heartExtraMarkerIcon':
+                params['icon'] = {
+                    type : 'extraMarker',
+                    icon: 'fa-heart',
+                    color: 'green',
+                    size: '1',
+                    shape: 'circle',
+                    prefix: 'fa'
+                }
+                createMarker(params);
+                break;
+            case 'homeExtraMarkerIcon':
+                params['icon'] = {
+                    type : 'extraMarker',
+                    icon: 'fa-home',
+                    color: 'green',
+                    size: '1',
+                    shape: 'circle',
+                    prefix: 'fa'
+                }
+                createMarker(params);
+                break;
+            case 'cogExtraMarkerIcon':
+                params['icon'] = {
+                    type : 'extraMarker',
+                    icon: 'fa-cog',
+                    color: 'green',
+                    size: '1',
+                    shape: 'circle',
+                    prefix: 'fa'
+                }
+                createMarker(params);
+                break;
+            case 'starExtraMarkerIcon':
+                params['icon'] = {
+                    type : 'extraMarker',
+                    icon: 'fa-star',
+                    color: 'green',
+                    size: '1',
+                    shape: 'circle',
+                    prefix: 'fa'
+                }
+                createMarker(params);
+                break;
+            case 'certificateExtraMarkerIcon':
+                params['icon'] = {
+                    type : 'extraMarker',
+                    icon: 'fa-certificate',
+                    color: 'green',
+                    size: '1',
+                    shape: 'circle',
+                    prefix: 'fa'
+                }
+                createMarker(params);
+                break;
+            case 'squareExtraMarkerIcon':
+                params['icon'] = {
+                    type : 'extraMarker',
+                    icon: 'fa-star',
+                    color: 'green',
+                    size: '1',
+                    shape: 'square',
+                    prefix: 'fa'
+                }
+                createMarker(params);
+                break;
+            case 'starExtraMarkerIcon':
+                params['icon'] = {
+                    type : 'extraMarker',
+                    icon: 'fa-star',
+                    color: 'green',
+                    size: '1',
+                    shape: 'star',
+                    prefix: 'fa'
+                }
+                createMarker(params);
+                break;
+            case 'circleExtraMarkerIcon':
+                params['icon'] = {
+                    type : 'extraMarker',
+                    icon: 'fa-star',
+                    color: 'green',
+                    size: '1',
+                    shape: 'circle',
+                    prefix: 'fa'
+                }
+                createMarker(params);
+                break;
+            default:
+                params['icon'] = {};
+                createMarker(params);
+                break;
+        }
+    }
 
+    function createMarker(obj){
+        angular.extend($scope,{
+            markers:{
+                m1:{
+                    lat: obj['lat'],
+                    lng: obj['lng'],
+                    message: obj['message'],
+                    focus: obj['focus'],
+                    icon: obj['icon']
+                }
+            }
+        });
 
+    }
     //var pos = L.GeoIP.getPosition();
     /*
     $scope.setRegion = function(region) {
