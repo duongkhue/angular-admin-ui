e024140eef4935f276b295165f694c0678f2611a
Duongkhue <duongkhue_c6@yahoo.com>
2015-06-10 10:42:55 +0700

    fix display layers and draw in map

--- src/app/modules/map/map.controller.js
+++ src/app/modules/map/map.controller.js
@@ -12,6 +12,45 @@ function mapController($scope, $http, leafletData){
     var vm = this;
     vm.titlepage = 'Map';
     vm.address = '1600 Pennsylvania Avenue NW, Washington, D.C. 20500';
+    angular.extend($scope, {
+        center: {
+            autoDiscover: true,
+            zoom: 20
+        },
+        controls: {
+            draw: {}
+        },
+        layers: {
+            baselayers: {
+                googleTerrain: {
+                    name: 'Google Terrain',
+                    layerType: 'TERRAIN',
+                    type: 'google'
+                },
+                googleHybrid: {
+                    name: 'Google Hybrid',
+                    layerType: 'HYBRID',
+                    type: 'google'
+                },
+                googleRoadmap: {
+                    name: 'Google Streets',
+                    layerType: 'ROADMAP',
+                    type: 'google'
+                }
+            }
+        }
+    });
+    leafletData.getMap().then(function(map) {
+        var drawnItems = $scope.controls.edit.featureGroup;
+        map.on('draw:created', function (e) {
+            var layer = e.layer;
+            drawnItems.addLayer(layer);
+            console.log(JSON.stringify(layer.toGeoJSON()));
+        });
+    });
+
+
+
     //var pos = L.GeoIP.getPosition();
     /*
     $scope.setRegion = function(region) {
@@ -21,31 +60,44 @@ function mapController($scope, $http, leafletData){
             $scope.maxbounds = regions[region];
         }
     };*/
-    angular.extend($scope,{
+    /*angular.extend($scope,{
         center:{
-            autoDiscover: true,
-            zoom:44
+            lat: 52.52,
+            lng: 13.40,
+            zoom: 14
         },
         controls: {
             draw: {}
         },
         layers: {
             baselayers: {
+                mapbox_light: {
+                    name: 'Mapbox Light',
+                    url: 'http://api.tiles.mapbox.com/v4/{mapid}/{z}/{x}/{y}.png?access_token={apikey}',
+                    type: 'xyz',
+                    layerOptions: {
+                        apikey: 'pk.eyJ1IjoiYnVmYW51dm9scyIsImEiOiJLSURpX0pnIn0.2_9NrLz1U9bpwMQBhVk97Q',
+                        mapid: 'bufanuvols.lia22g09'
+                    }
+                },
                 osm: {
                     name: 'OpenStreetMap',
                     url: 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
                     type: 'xyz'
-                },
-                mapbox_light: {
-                    url: 'http://api.tiles.mapbox.com/v4/{mapid}/{z}/{x}/{y}.png?access_token={apikey}',
-                    options: {
-                        apikey: 'pk.eyJ1IjoiYnVmYW51dm9scyIsImEiOiJLSURpX0pnIn0.2_9NrLz1U9bpwMQBhVk97Q',
-                        mapid: 'bufanuvols.lia22g09'
-                    }
                 }
+                //Google:L.Google()
+            }
+        },
+        markers:{
+            m1: {
+                lat: 52.52,
+                lng: 13.40,
+                draggable: true,
+                title:'test'
             }
         }
     });
+
     leafletData.getMap().then(function(map) {
         var drawnItems = $scope.controls.edit.featureGroup;
         map.on('draw:created', function (e) {
@@ -60,7 +112,7 @@ function mapController($scope, $http, leafletData){
                 autoDiscover: true
             }
         });
-    }
+    }*/
     /*L.GeoSearch.Provider.Google.GetLocations('Amsterdam', function(data){
         console.log(data);
     });*/
