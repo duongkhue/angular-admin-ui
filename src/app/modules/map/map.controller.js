4a5bca37b45c6b24a15be25d6943ab7569c76e0c
Duongkhue <duongkhue_c6@yahoo.com>
2015-06-11 18:18:43 +0700

    add routing machine

--- src/app/modules/map/map.controller.js
+++ src/app/modules/map/map.controller.js
@@ -12,6 +12,8 @@ function mapController($scope, $http, leafletData){
     var vm = this;
     vm.titlepage = 'Map';
     vm.address = '1600 Pennsylvania Avenue NW, Washington, D.C. 20500';
+
+
     leafletData.getMap('viewMap').then(function(map) {
         L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
@@ -28,6 +30,14 @@ function mapController($scope, $http, leafletData){
             }
         });
         map.addControl( new L.Control.Compass() );
+
+        L.Routing.control({
+            waypoints: [
+                L.latLng(57.74, 11.94),
+                L.latLng(57.6792, 11.949)
+            ],
+            routeWhileDragging: true
+        }).addTo(map);
     })
     angular.extend($scope, {
         center: {
@@ -415,15 +425,6 @@ function mapController($scope, $http, leafletData){
         });
 
     }
-    //var pos = L.GeoIP.getPosition();
-    /*
-    $scope.setRegion = function(region) {
-        if (!region) {
-            $scope.maxbounds = {};
-        } else {
-            $scope.maxbounds = regions[region];
-        }
-    };*/
     vm.findMe = function(){
         angular.extend($scope, {
             center: {
@@ -433,35 +434,19 @@ function mapController($scope, $http, leafletData){
     }
 
     vm.searchIP = function(obj){
-        var googleGeocodeProvider = new L.GeoSearch.Provider.Google(),
-            addressText = 'Amsterdam';
-        googleGeocodeProvider.GetLocations( addressText, function ( data ) {
-            console.log("data: " + data);
-            // in data are your results with x, y, label and bounds (currently availabel for google maps provider only)
+        getLatLng(obj.address);
+        if ($scope.latitude && $scope.longitude){
+            console.log($scope.latitude + ', ' + $scope.longitude);
+        }
+    }
+    function getLatLng(address){
+        var arr = [];
+        var geocoder = new google.maps.Geocoder();
+        geocoder.geocode( { 'address': address}, function(results, status) {
+            if (status == google.maps.GeocoderStatus.OK) {
+                $scope.latitude = results[0].geometry.location.lat();
+                $scope.longitude = results[0].geometry.location.lng();
+            }
         });
     }
-    /*vm.searchIP = function(obj){
-        var googleGeocodeProvider = new L.GeoSearch.Provider.Google(),
-            addressText = 'Amsterdam';
-
-        googleGeocodeProvider.GetLocations( addressText, function ( data ) {
-            // in data are your results with x, y, label and bounds (currently availabel for google maps provider only)
-        });
-        $scope.latitude = null;
-        $scope.longitude = null;
-        geocoderService.getLatLong(obj.address).then(function(latlng){
-            angular.extend($scope,{
-                center:{
-                    lat:$scope.latitude,
-                    lng:$scope.longitude,
-                    zoom:44
-                }
-            })
-        }, function(){
-            $scope.geocodeError = true;
-        }).finally(function(){
-            $scope.isFinished = true;
-        });
-        console.log('latitude: ' + $scope.latitude + ', longitude: ' + $scope.longitude);
-    }*/
 }
